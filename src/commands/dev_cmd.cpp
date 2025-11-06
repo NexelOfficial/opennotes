9705d1bd38e65cf468901508743965f2f6a881c9
NexelOfficial <nathandiepeveen.nl@gmail.com>
2025-11-06 16:54:24 +0100

    feat: Development server

--- src/commands/dev_cmd.cpp
+++ src/commands/dev_cmd.cpp
@@ -1,16 +1,20 @@
 #include <domino/global.h>
+#include <domino/nsfdb.h>
 #include <domino/nsferr.h>
+#include <domino/ostime.h>
+#include <ixwebsocket/IXWebSocketServer.h>
+#include <wincrypt.h>
+#include <winsock.h>
 
-#include <format>
-#include <future>
+#include <chrono>
 #include <iostream>
+#include <thread>
 
 #include "../command_handler.hpp"
 #include "../domino/database.hpp"
-#include "../domino/view.hpp"
 #include "../utils/error.hpp"
 #include "../utils/log.hpp"
-#include "../utils/parser.hpp"
+#include "ixwebsocket/IXWebSocket.h"
 
 static auto dev_cmd(const Args* args, Config* config) -> STATUS {
   // Get port, server and file from config
@@ -20,6 +24,62 @@ static auto dev_cmd(const Args* args, Config* config) -> STATUS {
     return (STATUS)NOERROR;
   }
 
+  std::vector<std::weak_ptr<ix::WebSocket>> clients = {};
+
+  ix::initNetSystem();
+  ix::WebSocketServer server(40456, "127.0.0.1");
+  server.setOnConnectionCallback(
+      [&server, &clients](std::weak_ptr<ix::WebSocket> webSocket,
+                          std::shared_ptr<ix::ConnectionState> connectionState) -> void {
+        clients.push_back(webSocket);
+
+        auto ws = webSocket.lock();
+        if (ws) {
+          ws->setOnMessageCallback(
+              [webSocket, connectionState,
+               &server](const std::unique_ptr<ix::WebSocketMessage>& msg) -> void { return; });
+        }
+      });
+
+  auto res = server.listenAndStart();
+
+  try {
+    const DatabaseInfo db_info = config->get_active_database();
+    const Database db = Database(db_info.server, db_info.file, db_info.port);
+    TIMEDATE previous_modified = {};
+
+    std::cout << "Development server running:\n> IP: http://127.0.0.1:40456/\n\n";
+
+    while (true) {
+      TIMEDATE now = {};
+      OSCurrentTIMEDATE(&now);
+
+      TIMEDATE last_modified = {};
+      NSFDbModifiedTime(db.get_handle(), nullptr, &last_modified);
+      bool is_same = (last_modified.Innards[0] == previous_modified.Innards[0]) &&
+                     (last_modified.Innards[1] == previous_modified.Innards[1]);
+
+      if (!is_same) {
+        previous_modified = last_modified;
+        std::cout << "Database change. Emitting to " << clients.size() << " client(s)...\n";
+
+        for (auto it = clients.begin(); it != clients.end();) {
+          if (it->expired()) {
+            it = clients.erase(it);
+          } else {
+            it->lock()->send("DB_UPDATE");
+            ++it;
+          }
+        }
+      }
+
+      std::this_thread::sleep_for(std::chrono::milliseconds(200));
+    }
+  } catch (const NotesException& ex) {
+    Log::error(ex.what());
+  }
+
+  ix::uninitNetSystem();
   return 0;
 }
 
